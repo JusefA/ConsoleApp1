2c2f8c7b6b76e633034f47044173556a8e613af7
Jusef Al-itawa <jusef03@gmail.com>
2023-01-04 15:40:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yourName;
+            Console.WriteLine("what is your name?");
+
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("hello {0}", yourName);
+        }
+    }
+}
